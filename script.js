6ff66b052815ae98489ffd4a9d75894a97c465e1
MarkVanweersch <mark-vanweersch@live.nl>
2022-10-26 20:39:58 +0200

    New projects added

--- script.js
+++
@@ -1,9 +0,0 @@
-/* window.onload = onLoad;
-
-function onLoad() {
-  document.querySelector(".language-container").addEventListener("click", changeLanguage);
-
-  function changeLanguage() {
-    
-  }
-} */
